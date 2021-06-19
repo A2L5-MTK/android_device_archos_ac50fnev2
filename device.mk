83a9ef7891212a536cd2e2385caed2917dcbf9b9
A2L5E0X1 <alexed25@protonmail.com>
2021-06-19 22:13:29 +0200

    ac50fnev2: build basic packages
    Change-Id: Iab81d0f4895d6ac2f5a2143e6c69ae56d084a5ce

--- device.mk
+++ device.mk
@@ -20,9 +20,46 @@ DEVICE_PATH := device/archos/ac50fnev2
 ## Vendor Blobs
 $(call inherit-product, vendor/archos/ac50fnev2/ac50fnev2-vendor.mk)
 
+# Audio
+PRODUCT_PACKAGES += \
+    audio.r_submix.default \
+    audio.a2dp.default \
+    audio.usb.default \
+    libaudio-resampler \
+    libtinyalsa \
+    libtinycompress \
+    libtinymix \
+    libtinyxml
+
+PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:system/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:system/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/etc/usb_audio_policy_configuration.xml
+
 # Display
 TARGET_SCREEN_HEIGHT := 854
 TARGET_SCREEN_WIDTH := 480
+
+# FMRadio
+PRODUCT_PACKAGES += \
+    libfmjni \
+    FMRadio
+
+# GPS
+PRODUCT_PACKAGES += \
+    libcurl
+
+# Graphics
+PRODUCT_PACKAGES += \
+    libion
+
+# Media
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
 
 # Rootdir
 PRODUCT_PACKAGES += \
@@ -36,3 +73,17 @@ PRODUCT_PACKAGES += \
     init.project.rc \
     init.rilproxy.rc \
     ueventd.mt6580.rc
+
+# Snap
+PRODUCT_PACKAGES += \
+    Snap
+
+# USB
+PRODUCT_PACKAGES += \
+    com.android.future.usb.accessory
+
+# Wifi
+PRODUCT_PACKAGES += \
+    libwpa_client \
+    hostapd \
+    wpa_supplicant
